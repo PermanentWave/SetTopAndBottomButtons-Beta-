3259a96e63ea6b16d206dd6380e147ca8c630062
PermanentWave <68002384+PermanentWave@users.noreply.github.com>
2020-07-25 13:32:01 +0900

    Update Set Top and Bottom Buttons.js

--- Set Top and Bottom Buttons.js
+++ Set Top and Bottom Buttons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Set Top and Bottom buttons (Beta)
 // @description Set Top and Bottom buttons on your browser (Beta)
-// @version 1.07b19
+// @version 1.07b20
 // @author PermanentWave
 // @license Copyright (c) 2020 PermanentWave Released under the MIT license https://opensource.org/licenses/mit-license.php
 // @include *
