b704f43d567f39d7ca4823c0d314b0d263ee0c1d
PermanentWave <okuya.nemesis@gmail.com>
2020-07-26 20:02:08 +0900

    Add Comment

--- Set Top and Bottom Buttons.js
+++ Set Top and Bottom Buttons.js
@@ -28,19 +28,23 @@ const varIdleTimeOut = 3000; // edit this value
 const varAutoHideMode = true; // edit this value
 
 // [1] skip all iframe 
-if (window.self!=window.top) {return}
+if (window.self!=window.top) { return; }
 
 // create element
 function fncCreateElement(varNumber) { return document.createElement(varNumber); } // end of function
 
 // select element
 function fncSelectElement() {
+	// high priority used
 	if ('scrollingElement' in document) {
 		return document.scrollingElement;
+	// firefox
 	} else if (navigator.userAgent.indexOf('webkit') != -1) {
 		return document.body;
+	// browser expected firefox
 	} else if ('documentElement' in document) {
 		return document.documentElement;
+	// default
 	} else {
 		return document.body;
 	} // end if
@@ -141,9 +145,9 @@ function fncShareCSS(){
 	varImgDown = 'data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAACPSURBVDhPY2DAAlatWvUfH8amB6vYqEGEg2pgw4iQ7cTKM6xcuXImsYpxqQOZAQ4woIIOCgzrQAl1oEFpZBiWhitFgwx7R4SBIDXYDYGZDFRgTMAwkCHGhBMRJMxwGUa8ITCbli5dKgg08AySN8+AxIhyCboiJMPIN4Qsm6miiYioxltawvSDYogohYTUAQC80UNTOht/YwAAAABJRU5ErkJggg==';
 	varImgCheck = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsEAAA7BAbiRa+0AAABTSURBVDhPY/z//z8DNQATlKYYEDRo9erV/0EYysUJqOaiIRhGxAKsXsMWuKGhoYxQJlZAv8CGuY5uLho1iDAYfAYRnbJhAFd6ornX2PFgLICBAQBQciFGldEVwAAAAABJRU5ErkJggg=='; // beta version only
 	// button id
-	varString+='#play_btn_up { position:fixed; right:0; bottom:' + varYPosition + '%;z-index:'+varZIndex+'; height:36px; width:36px; cursor:pointer; background:url('+varImgUp+') no-repeat scroll 50% 50% rgba(0, 0, 0, 0.7); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
-	varString+='#play_btn_dn { position:fixed; right:0; top:' + varYPosition + '%;   z-index:'+varZIndex+'; height:36px; width:36px; cursor:pointer; background:url('+varImgDown+') no-repeat scroll 50% 50% rgba(0, 0, 0, 0.7); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
-	varString+='#play_btn_chk { position:fixed; right:0; top:' + (varYPosition + 15) + '%;   z-index:'+varZIndex+'; height:36px; width:36px; cursor:pointer; background:url('+varImgCheck+') no-repeat scroll 50% 50% rgba(0, 0, 0, 0.7); border-radius:5px 0 0 5px; margin-top:-24px; }'; // beta version only
+	varString+='#play_btn_up { position:fixed; right:0; bottom:' + varYPosition + '%;z-index:'+varZIndex+'; height:36px; width:36px; cursor:pointer; background:url(' + varImgUp + ') no-repeat scroll 50% 50% rgba(0, 0, 0, 0.7); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
+	varString+='#play_btn_dn { position:fixed; right:0; top:' + varYPosition + '%;   z-index:'+varZIndex+'; height:36px; width:36px; cursor:pointer; background:url(' + varImgDown + ') no-repeat scroll 50% 50% rgba(0, 0, 0, 0.7); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
+	varString+='#play_btn_chk { position:fixed; right:0; top:' + (varYPosition + 15) + '%;   z-index:'+varZIndex+'; height:36px; width:36px; cursor:pointer; background:url(' + varImgCheck + ') no-repeat scroll 50% 50% rgba(0, 0, 0, 0.7); border-radius:5px 0 0 5px; margin-top:-24px; }'; // beta version only
 	// button class
 	varString+='.play_btn { -webkit-transition-duration:0.5s linear; -o-transition-duration:0.5s linear; -moz-transition-duration:0.5s linear; transition-duration:0.5s linear; opacity:0.65; }'; 
 	varString+='.play_btn:hover { opacity:1; }'; 
@@ -199,7 +203,7 @@ function fncCreateButtonElement() {
 				varUpButton.style.display = "none";
 				varDownButton.style.display = "none";
 				varCheckButton.style.display = "none"; // beta version only
-			}
+			} // end if
 			return true;
 		}; // end function
 
@@ -229,10 +233,10 @@ function fncCreateButtonElement() {
 			varCheckButton.style.display = ""; // beta version only
 			
 			// if click
-			if (varClickFlag < 0) {
+			if ( varClickFlag < 0 ) {
 				varUpButton.style.display = "none";
 				varClickFlag = 0;
-			} else if (varClickFlag > 0) {
+			} else if ( varClickFlag > 0 ) {
 				varDownButton.style.display = "none";
 				varClickFlag = 0;
 			} // end if
@@ -244,13 +248,13 @@ function fncCreateButtonElement() {
 		// add event click
 		varUpButton.addEventListener('click', fncRestartTimer, false);
 		varUpButton.addEventListener('click', fncMoveUp, false);
-		varUpButton.addEventListener('click', function(){clearTimeout(varUpTimer);}, false);
-		varUpButton.addEventListener('click', function(){varClickFlag = -1;}, false);
+		varUpButton.addEventListener('click', function(){ clearTimeout(varUpTimer); }, false);
+		varUpButton.addEventListener('click', function(){ varClickFlag = -1; }, false);
 		
 		varDownButton.addEventListener('click', fncRestartTimer, false);
 		varDownButton.addEventListener('click', fncMoveDown, false);
-		varDownButton.addEventListener('click', function(){clearTimeout(varDownTimer);}, false);
-		varDownButton.addEventListener('click', function(){varClickFlag = 1;}, false);
+		varDownButton.addEventListener('click', function(){ clearTimeout(varDownTimer); }, false);
+		varDownButton.addEventListener('click', function(){ varClickFlag = 1; }, false);
 		
 		varCheckButton.addEventListener('click', fncRestartTimer, false); // beta version only
 		varCheckButton.addEventListener('click', fncCheckHeight, false); // beta version only
