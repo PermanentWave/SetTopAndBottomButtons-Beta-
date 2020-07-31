aa2fef5115090c358557704ac5fce5d7876dc25f
PermanentWave <okuya.nemesis@gmail.com>
2020-07-31 17:48:30 +0900

    Optimize

--- Set Top and Bottom Buttons.js
+++ Set Top and Bottom Buttons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Set Top and Bottom buttons (Beta)
 // @description Set Top and Bottom buttons on your browser
-// @version 1.09b6
+// @version 1.10b1
 // @author PermanentWave
 // @license Copyright (c) 2020 PermanentWave Released under the MIT license https://github.com/PermanentWave/SetTopAndBottomButtons/blob/master/LICENSE
 // @include *
