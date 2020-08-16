c889d2d93f40643f1f8a51caf9eb667fb1cdea51
PermanentWave <okuya.nemesis@gmail.com>
2020-08-17 08:12:37 +0900

    change filename

--- Set Top and Bottom Buttons.js
+++ Set Top and Bottom Buttons.js
@@ -6,7 +6,7 @@
 // @version 1.12b4
 // @author PermanentWave
 // @homepageURL https://github.com/PermanentWave/SetTopAndBottomButtons
-// @updateURL https://github.com/PermanentWave/SetTopAndBottomButtons-Beta-/SetTopAndBottomButtons.user.js
+// @updateURL https://github.com/PermanentWave/SetTopAndBottomButtons-Beta-/raw/master/SetTopandBottomButtons.user.js
 // @supportURL https://github.com/PermanentWave/SetTopAndBottomButtons/issues
 // @license Copyright (c) 2020 PermanentWave Released under the MIT license https://github.com/PermanentWave/SetTopAndBottomButtons/blob/master/LICENSE
 // @include *
