5ab1ab04f77d85b257efb081c075a39d2d09c9a4
PermanentWave <okuya.nemesis@gmail.com>
2020-08-25 23:19:22 +0900

    Optimize

--- Old Version/SetTopAndBottomButtons ver2.0b1.user.js
+++ Old Version/SetTopAndBottomButtons ver2.0b1.user.js
@@ -3,7 +3,7 @@
 // @name:ja AdGuard 最上部/最下部 移動ボタン (Beta)
 // @description Set Top and Bottom buttons on your browser (Beta)
 // @description:ja 最上部/最下部へ移動するボタンをブラウザ上に追加します (Beta)
-// @version 1.12b4
+// @version 2.0b1
 // @author PermanentWave
 // @homepageURL https://github.com/PermanentWave/SetTopAndBottomButtons
 // @updateURL https://github.com/PermanentWave/SetTopAndBottomButtons-Beta-/raw/master/SetTopandBottomButtons.user.js
@@ -19,13 +19,10 @@
 function SetTopBottomButtons( ) {
 
 	// load element
-	const letElement = fncSelectElement( );
+	const constElement = fncSelectElement( );
 	// timer
-	let letUpTimer;
-	let letDownTimer;
 	let letIdleTimer;
-	// up and down timeout (miliseconds)
-	const constTimeOut = 0;
+
 	// z-index (layer number)
 	const constZIndex = 1001; // edit this value
 	// y-position (%)
@@ -34,6 +31,10 @@ function SetTopBottomButtons( ) {
 	const constIdleTimeOut = 2000; // edit this value
 	// auto hide buttons (true:auto hide, false:always show)
 	const constAutoHideMode = true; // edit this value
+	// system value
+	const GO_TO_TOP = -1;
+	const GO_TO_BOTTOM = 1;
+	const NOT_WORKING = 0;
 
 	// create element
 	function fncCreateElement( varNumber ) { return document.createElement( varNumber ); }; // end of function
@@ -70,39 +71,23 @@ function SetTopBottomButtons( ) {
 
 	// move up
 	function fncMoveUp( ) { 
-		window.scrollTo( 0, 0 );
-		letUpTimer = setTimeout( fncMoveUp, constTimeOut );
+		window.scrollBy( 0, -fncScrollTop( constElement ) );
 
 		return true;
 	}; // end of function
 
 	// move down
 	function fncMoveDown( ) { 
-		let letDocumentHeight = fncSelectElement( );
-		let letBottom = letDocumentHeight.scrollHeight - letDocumentHeight.clientHeight;
-		window.scrollTo( 0, letBottom );
-		letDownTimer = setTimeout( fncMoveDown, constTimeOut );
+		window.scrollTo( 0, fncScrollBottom( constElement ) );
 
 		return true;
 	}; // end of function
 
-	// get y-position from top
-	function fncScrollTop( varDocumentElement ) {
-		let letScrollTop = window.pageYOffset || varDocumentElement.scrollTop;
+	// get y-position from top to client top
+	function fncScrollTop( varDocumentElement ) { return window.pageYOffset || varDocumentElement.scrollTop; }; // end of function
 
-		return letScrollTop;
-	}; // end of function
-
-	// get y-position from bottom
-	function fncScrollBottom( varDocumentElement ) {
-		let letScrollTop = fncScrollTop( varDocumentElement );
-		let letScrollHeight = varDocumentElement.scrollHeight;
-		let letClientHeight = varDocumentElement.clientHeight;
-		let letClientTop = varDocumentElement.clientTop;
-		let letScrollBottom = letScrollHeight - letClientHeight - letClientTop - letScrollTop;
-
-		return letScrollBottom;
-	}; // end of function
+	// get y-position from bottom to client bottom
+	function fncScrollBottom( varDocumentElement ) { return varDocumentElement.scrollHeight - varDocumentElement.clientHeight - fncScrollTop( varDocumentElement ); }; // end of function
 
 	// Height check		// only beta version
 	function fncCheckHeight( ) {
@@ -155,7 +140,7 @@ function SetTopBottomButtons( ) {
 	}; // end of function
 
 	// document scroll
-	function fncGetScroll( ) { return ( letElement.clientHeight < letElement.scrollHeight ); }; // end of function
+	function fncGetScroll( ) { return ( constElement.clientHeight < constElement.scrollHeight ); }; // end of function
 
 	// add css
 	function fncShareCSS( ) { 
@@ -166,8 +151,8 @@ function SetTopBottomButtons( ) {
 		let letImgCheck; // beta version only
 
 		// img vs button
-		letImgUp = 'data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAB+SURBVDhPY1i1atV/amAGahgCMoNhaIGlS5cKAp19BoRBbLJcj2QILDJINwzoAmMgfoclIkBixkS5DI8hMJcRNgxoSBoOl6CnNZBhaVhdBjWE1MSJahjQkA4KEmYH2GUrV66cSYEhYB+AzKBtFiHkQqKiH6Ro1CDCQTWgYQQAs81DU0G/83sAAAAASUVORK5CYII=';
-		letImgDown = 'data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAACPSURBVDhPY2DAAlatWvUfH8amB6vYqEGEg2pgw4iQ7cTKM6xcuXImsYpxqQOZAQ4woIIOCgzrQAl1oEFpZBiWhitFgwx7R4SBIDXYDYGZDFRgTMAwkCHGhBMRJMxwGUa8ITCbli5dKgg08AySN8+AxIhyCboiJMPIN4Qsm6miiYioxltawvSDYogohYTUAQC80UNTOht/YwAAAABJRU5ErkJggg==';
+		letImgUp = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj4ojpwAAAAWdFJOUwCPXbw09KcO/gafzs9HdHUY3upIu0YxVMnGAAAAd0lEQVQY03WQWxKAIAhFMV9ob639b7WMTKiJH5jLHQ4A8B/ev5VlXZeXZ0ipF76grTFWByY53AFGdE1R2JXUoarKjBMRr+JpEvG25zKiEh2OAMZuoRFLAZGaleiHeI3lRDrsAUmiWDDzpRtRHEtE/pL4ed33lWccXp0Ehz6OBLwAAAAASUVORK5CYII=';
+		letImgDown = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj4ojpwAAAAWdFJOUwD0R6cGArz+DRVcdM6PNDXe6p7Pr5BnxQpHAAAAdklEQVQY03WQ2xKAIAhEKTXEtLTL//9qY6SJTfsiLsweBeArsvJuLRxeeHbfYB0m/Tp6GhaAEZUpjlE45tNhKNaJjosZZ1FkYuDmPf4QOeIOrcQMqkVp+pTq+BNBMVJPJGrQkige3X4DXmK7ACa2MkzsVknwrwutXwTWIXkOZgAAAABJRU5ErkJggg==';
 		letImgCheck = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsEAAA7BAbiRa+0AAABTSURBVDhPY/z//z8DNQATlKYYEDRo9erV/0EYysUJqOaiIRhGxAKsXsMWuKGhoYxQJlZAv8CGuY5uLho1iDAYfAYRnbJhAFd6ornX2PFgLICBAQBQciFGldEVwAAAAABJRU5ErkJggg=='; // beta version only
 		// button id
 		letString += '#play_btn_up { position:fixed; right:0; bottom:' + constYPosition + '%;z-index:' + constZIndex + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgUp + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
@@ -187,18 +172,15 @@ function SetTopBottomButtons( ) {
 		let letUpButton;
 		let letDownButton;
 		let letCheckButton; // beta version only
-		let letHeight = fncGetScroll( );
-		let letClickFlag = 0;
+		let letScroll = fncGetScroll( );
+		let letClickFlag = NOT_WORKING;
 
 		// switch visible buttons
 		function fncVisibleButtons( ) {
-			let letScrollTop = fncScrollTop( letElement );
-			let letScrollBottom = fncScrollBottom( letElement );
-
 			// if scroll up
-			letUpButton.style.display = ( letScrollTop > 0 ) ? "" : "none";
+			letUpButton.style.display = ( fncScrollTop( constElement ) > 0 ) ? "" : "none";
 			// if scroll down
-			letDownButton.style.display = ( letScrollBottom >= 1 ) ? "" : "none"; // remove digits after decimal point
+			letDownButton.style.display = ( fncScrollBottom( constElement ) >= 1 ) ? "" : "none"; // remove digits after decimal point
 			// always on
 			letCheckButton.style.display = ""; // beta version only
 
@@ -218,7 +200,6 @@ function SetTopBottomButtons( ) {
 
 		// reset timer
 		function fncRestartTimer( ) {
-			fncVisibleButtons( );
 			clearTimeout( letIdleTimer );
 			letIdleTimer = setTimeout( fncInvisibleButtons, constIdleTimeOut );
 
@@ -230,25 +211,25 @@ function SetTopBottomButtons( ) {
 			fncVisibleButtons( );
 
 			// if click
-			if ( letClickFlag < 0 ) { // invisible top button if click top button
+			if ( letClickFlag = GO_TO_TOP ) { // invisible top button if click top button
 				letUpButton.style.display = "none";
-				letClickFlag = 0;
-			} else if ( letClickFlag > 0 ) { // invisible buttom button if click buttom button
+				letClickFlag = NOT_WORKING;
+			} else if ( letClickFlag = GO_TO_BOTTOM ) { // invisible buttom button if click buttom button
 				letDownButton.style.display = "none";
-				letClickFlag = 0;
+				letClickFlag = NOT_WORKING;
 			} // end if
 
 			return true;
 		}; // end function
 
 		// exit function
-		if( !letHeight ) { return; }; // end if
+		if( !letScroll ) { return; }; // end if
 
 		// add css
 		fncShareCSS( ); 
 		
 		// if loading element
-		if( letElement ) { 
+		if( constElement ) { 
 			// create DOM element
 			letUpButton = fncCreateElement( 'span' );
 			letDownButton = fncCreateElement( 'span' );
@@ -266,18 +247,17 @@ function SetTopBottomButtons( ) {
 			document.body.appendChild( letDownButton );
 			document.body.appendChild( letCheckButton ); // beta version only
 
+			// initialize buttons
 			fncVisibleButtons( );
 
 			// add event loading
 			window.addEventListener( 'load', fncRestartTimer, false );
 			// add event click up button
 			letUpButton.addEventListener( 'click', fncMoveUp, false );
-			letUpButton.addEventListener( 'click', function( ){ clearTimeout( letUpTimer ); }, false );
-			letUpButton.addEventListener( 'click', function( ){ letClickFlag = -1; }, false );
+			letUpButton.addEventListener( 'click', function( ){ letClickFlag = GO_TO_TOP; }, false );
 			// add event click down button
 			letDownButton.addEventListener( 'click', fncMoveDown, false );
-			letDownButton.addEventListener( 'click', function( ){ clearTimeout( letDownTimer ); }, false );
-			letDownButton.addEventListener( 'click', function( ){ letClickFlag = 1; }, false );
+			letDownButton.addEventListener( 'click', function( ){ letClickFlag = GO_TO_BOTTOM; }, false );
 			// add event click information button
 			letCheckButton.addEventListener( 'click', fncCheckHeight, false ); // beta version only
 			// add event scroll
@@ -291,7 +271,7 @@ function SetTopBottomButtons( ) {
 	// skip all iframe 
 	if ( window.self != window.top ) { return; };
 
-	// run it
+	// run
 	fncCreateButtonElement( );
 
 	return true;
--- SetTopAndBottomButtons.user.js
+++ SetTopAndBottomButtons.user.js
@@ -3,10 +3,10 @@
 // @name:ja AdGuard 最上部/最下部 移動ボタン (Beta)
 // @description Set Top and Bottom buttons on your browser (Beta)
 // @description:ja 最上部/最下部へ移動するボタンをブラウザ上に追加します (Beta)
-// @version 1.13b3
+// @version 2.0b1
 // @author PermanentWave
 // @homepageURL https://github.com/PermanentWave/SetTopAndBottomButtons
-// @updateURL https://raw.githubusercontent.com/PermanentWave/SetTopAndBottomButtons-Beta-/master/SetTopAndBottomButtons.user.js
+// @updateURL https://github.com/PermanentWave/SetTopAndBottomButtons-Beta-/raw/master/SetTopandBottomButtons.user.js
 // @supportURL https://github.com/PermanentWave/SetTopAndBottomButtons/issues
 // @license Copyright (c) 2020 PermanentWave Released under the MIT license https://github.com/PermanentWave/SetTopAndBottomButtons/blob/master/LICENSE
 // @include *
@@ -19,13 +19,10 @@
 function SetTopBottomButtons( ) {
 
 	// load element
-	const letElement = fncSelectElement( );
+	const constElement = fncSelectElement( );
 	// timer
-	let letUpTimer;
-	let letDownTimer;
 	let letIdleTimer;
-	// up and down timeout (miliseconds)
-	const constTimeOut = 0;
+
 	// z-index (layer number)
 	const constZIndex = 1001; // edit this value
 	// y-position (%)
@@ -34,6 +31,10 @@ function SetTopBottomButtons( ) {
 	const constIdleTimeOut = 2000; // edit this value
 	// auto hide buttons (true:auto hide, false:always show)
 	const constAutoHideMode = true; // edit this value
+	// system value
+	const GO_TO_TOP = -1;
+	const GO_TO_BOTTOM = 1;
+	const NOT_WORKING = 0;
 
 	// create element
 	function fncCreateElement( varNumber ) { return document.createElement( varNumber ); }; // end of function
@@ -70,40 +71,23 @@ function SetTopBottomButtons( ) {
 
 	// move up
 	function fncMoveUp( ) { 
-		window.scrollTo( 0, 0 );
-		letUpTimer = setTimeout( fncMoveUp, constTimeOut );
+		window.scrollBy( 0, -fncScrollTop( constElement ) );
 
 		return true;
 	}; // end of function
 
 	// move down
 	function fncMoveDown( ) { 
-		let letDocumentHeight = fncSelectElement( );
-		let letBottom = letDocumentHeight.scrollHeight - letDocumentHeight.clientHeight;
-
-		window.scrollTo( 0, letBottom );
-		letDownTimer = setTimeout( fncMoveDown, constTimeOut );
+		window.scrollTo( 0, fncScrollBottom( constElement ) );
 
 		return true;
 	}; // end of function
 
-	// get y-position from top
-	function fncScrollTop( varDocumentElement ) {
-		let letScrollTop = window.pageYOffset || varDocumentElement.scrollTop;
+	// get y-position from top to client top
+	function fncScrollTop( varDocumentElement ) { return window.pageYOffset || varDocumentElement.scrollTop; }; // end of function
 
-		return letScrollTop;
-	}; // end of function
-
-	// get y-position from bottom
-	function fncScrollBottom( varDocumentElement ) {
-		let letScrollTop = fncScrollTop( varDocumentElement );
-		let letScrollHeight = varDocumentElement.scrollHeight;
-		let letClientHeight = varDocumentElement.clientHeight;
-		let letClientTop = varDocumentElement.clientTop;
-		let letScrollBottom = letScrollHeight - letClientHeight - letClientTop - letScrollTop;
-
-		return letScrollBottom;
-	}; // end of function
+	// get y-position from bottom to client bottom
+	function fncScrollBottom( varDocumentElement ) { return varDocumentElement.scrollHeight - varDocumentElement.clientHeight - fncScrollTop( varDocumentElement ); }; // end of function
 
 	// Height check		// only beta version
 	function fncCheckHeight( ) {
@@ -156,7 +140,7 @@ function SetTopBottomButtons( ) {
 	}; // end of function
 
 	// document scroll
-	function fncGetScroll( ) { return ( letElement.clientHeight < letElement.scrollHeight ); }; // end of function
+	function fncGetScroll( ) { return ( constElement.clientHeight < constElement.scrollHeight ); }; // end of function
 
 	// add css
 	function fncShareCSS( ) { 
@@ -167,8 +151,8 @@ function SetTopBottomButtons( ) {
 		let letImgCheck; // beta version only
 
 		// img vs button
-		letImgUp = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAVUExURaqqqkdwTKqqqqqqqqqqqqqqqqqqqrPOA6gAAAAHdFJOUwMAoDTvs5Tx1j0eAAAAVElEQVQY053QMQ7AIAwEwT0M/P/JQdgQo0iRkitcTLGF0WN8InrnJEqthUxDzJaxRFrGlm2EzLwbIZ6fhkI8P46gWc5b4yWv20iinFcyz6dF/tcLL4iHAhVZbVsUAAAAAElFTkSuQmCC';
-		letImgDown = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAbUExURUdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqorZq28AAAAJdFJOUwADoDTvCJSzxZRnqR4AAABPSURBVBjTpcwxEsAgCETRRdTk/icOimYxRQql2Ble8YHdK7fGV68CpBxMc7KVYCaCxabQprS8W5c338yFeZshzEut8pOnRQHzWOwj8PzhPRQ2AWOOEzeLAAAAAElFTkSuQmCC';
+		letImgUp = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj4ojpwAAAAWdFJOUwCPXbw09KcO/gafzs9HdHUY3upIu0YxVMnGAAAAd0lEQVQY03WQWxKAIAhFMV9ob639b7WMTKiJH5jLHQ4A8B/ev5VlXZeXZ0ipF76grTFWByY53AFGdE1R2JXUoarKjBMRr+JpEvG25zKiEh2OAMZuoRFLAZGaleiHeI3lRDrsAUmiWDDzpRtRHEtE/pL4ed33lWccXp0Ehz6OBLwAAAAASUVORK5CYII=';
+		letImgDown = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABCUExURUdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj4ojpwAAAAWdFJOUwD0R6cGArz+DRVcdM6PNDXe6p7Pr5BnxQpHAAAAdklEQVQY03WQ2xKAIAhEKTXEtLTL//9qY6SJTfsiLsweBeArsvJuLRxeeHbfYB0m/Tp6GhaAEZUpjlE45tNhKNaJjosZZ1FkYuDmPf4QOeIOrcQMqkVp+pTq+BNBMVJPJGrQkige3X4DXmK7ACa2MkzsVknwrwutXwTWIXkOZgAAAABJRU5ErkJggg==';
 		letImgCheck = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsEAAA7BAbiRa+0AAABTSURBVDhPY/z//z8DNQATlKYYEDRo9erV/0EYysUJqOaiIRhGxAKsXsMWuKGhoYxQJlZAv8CGuY5uLho1iDAYfAYRnbJhAFd6ornX2PFgLICBAQBQciFGldEVwAAAAABJRU5ErkJggg=='; // beta version only
 		// button id
 		letString += '#play_btn_up { position:fixed; right:0; bottom:' + constYPosition + '%;z-index:' + constZIndex + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgUp + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
@@ -188,18 +172,15 @@ function SetTopBottomButtons( ) {
 		let letUpButton;
 		let letDownButton;
 		let letCheckButton; // beta version only
-		let letHeight = fncGetScroll( );
-		let letClickFlag = 0;
+		let letScroll = fncGetScroll( );
+		let letClickFlag = NOT_WORKING;
 
 		// switch visible buttons
 		function fncVisibleButtons( ) {
-			let letScrollTop = fncScrollTop( letElement );
-			let letScrollBottom = fncScrollBottom( letElement );
-
 			// if scroll up
-			letUpButton.style.display = ( letScrollTop > 0 ) ? "" : "none";
+			letUpButton.style.display = ( fncScrollTop( constElement ) > 0 ) ? "" : "none";
 			// if scroll down
-			letDownButton.style.display = ( letScrollBottom >= 1 ) ? "" : "none"; // remove digits after decimal point
+			letDownButton.style.display = ( fncScrollBottom( constElement ) >= 1 ) ? "" : "none"; // remove digits after decimal point
 			// always on
 			letCheckButton.style.display = ""; // beta version only
 
@@ -219,7 +200,6 @@ function SetTopBottomButtons( ) {
 
 		// reset timer
 		function fncRestartTimer( ) {
-			fncVisibleButtons( );
 			clearTimeout( letIdleTimer );
 			letIdleTimer = setTimeout( fncInvisibleButtons, constIdleTimeOut );
 
@@ -231,25 +211,25 @@ function SetTopBottomButtons( ) {
 			fncVisibleButtons( );
 
 			// if click
-			if ( letClickFlag < 0 ) { // invisible top button if click top button
+			if ( letClickFlag = GO_TO_TOP ) { // invisible top button if click top button
 				letUpButton.style.display = "none";
-				letClickFlag = 0;
-			} else if ( letClickFlag > 0 ) { // invisible buttom button if click buttom button
+				letClickFlag = NOT_WORKING;
+			} else if ( letClickFlag = GO_TO_BOTTOM ) { // invisible buttom button if click buttom button
 				letDownButton.style.display = "none";
-				letClickFlag = 0;
+				letClickFlag = NOT_WORKING;
 			} // end if
 
 			return true;
 		}; // end function
 
 		// exit function
-		if( !letHeight ) { return; }; // end if
+		if( !letScroll ) { return; }; // end if
 
 		// add css
 		fncShareCSS( ); 
 		
 		// if loading element
-		if( letElement ) { 
+		if( constElement ) { 
 			// create DOM element
 			letUpButton = fncCreateElement( 'span' );
 			letDownButton = fncCreateElement( 'span' );
@@ -267,18 +247,17 @@ function SetTopBottomButtons( ) {
 			document.body.appendChild( letDownButton );
 			document.body.appendChild( letCheckButton ); // beta version only
 
+			// initialize buttons
 			fncVisibleButtons( );
 
 			// add event loading
 			window.addEventListener( 'load', fncRestartTimer, false );
 			// add event click up button
 			letUpButton.addEventListener( 'click', fncMoveUp, false );
-			letUpButton.addEventListener( 'click', function( ){ clearTimeout( letUpTimer ); }, false );
-			letUpButton.addEventListener( 'click', function( ){ letClickFlag = -1; }, false );
+			letUpButton.addEventListener( 'click', function( ){ letClickFlag = GO_TO_TOP; }, false );
 			// add event click down button
 			letDownButton.addEventListener( 'click', fncMoveDown, false );
-			letDownButton.addEventListener( 'click', function( ){ clearTimeout( letDownTimer ); }, false );
-			letDownButton.addEventListener( 'click', function( ){ letClickFlag = 1; }, false );
+			letDownButton.addEventListener( 'click', function( ){ letClickFlag = GO_TO_BOTTOM; }, false );
 			// add event click information button
 			letCheckButton.addEventListener( 'click', fncCheckHeight, false ); // beta version only
 			// add event scroll
@@ -292,7 +271,7 @@ function SetTopBottomButtons( ) {
 	// skip all iframe 
 	if ( window.self != window.top ) { return; };
 
-	// run it
+	// run
 	fncCreateButtonElement( );
 
 	return true;
