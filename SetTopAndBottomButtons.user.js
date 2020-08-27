944cb7aaffc91b8e7c8c69547914d53d0241d087
PermanentWave <okuya.nemesis@gmail.com>
2020-08-27 20:32:25 +0900

    Optimize

--- SetTopAndBottomButtons.user.js
+++ SetTopAndBottomButtons.user.js
@@ -3,7 +3,7 @@
 // @name:ja AdGuard 最上部/最下部 移動ボタン (Beta)
 // @description Set Top and Bottom buttons on your browser (Beta)
 // @description:ja 最上部/最下部へ移動するボタンをブラウザ上に追加します (Beta)
-// @version 2.0b3
+// @version 2.0b4
 // @author PermanentWave
 // @homepageURL https://github.com/PermanentWave/SetTopAndBottomButtons
 // @updateURL https://raw.githubusercontent.com/PermanentWave/SetTopAndBottomButtons-Beta-/master/SetTopAndBottomButtons.user.js
@@ -20,15 +20,15 @@ function SetTopBottomButtons( ) {
 
 	// timer
 	let letIdleTimer;
-	// load element
-	const LOADING_ELEMENT = fncSelectElement( );
-	// z-index (layer number)
+	// loaded element
+	const LOAD_ELEMENT = fncSelectElement( );
+	// layer index (number)
 	const LAYER_INDEX = 1001; // edit this value
-	// y-position (%)
-	const Y_POSITION = 55; // edit this value
-	const Y_POSITION_OFFSET = 15; // edit this value
+	// position (%)
+	const POSITION = 55; // edit this value
+	const POSITION_OFFSET = 15; // edit this value	// only beta version
 	// idle timeout (milliseconds)
-	const IDLE_TIME_OUT = 2000; // edit this value
+	const IDLE_TIMEOUT = 2000; // edit this value
 	// auto hide buttons (true:auto hide, false:always show)
 	const AUTO_HIDE_MODE = true; // edit this value
 
@@ -54,37 +54,64 @@ function SetTopBottomButtons( ) {
 
 	// add style
 	function fncAddStyle( varCSS ) {
-		let letHtmlHeadElement = document.head || document.getElementsByTagName( 'head' )[0];
-		if ( letHtmlHeadElement ) {
+		let letElement = document.head || document.getElementsByTagName( 'head' )[0];
+		if ( letElement ) {
 			let letStyle = fncCreateElement( "style" );
 			letStyle.type = "text/css";
 			letStyle.appendChild( document.createTextNode( varCSS ) );
-			letHtmlHeadElement.appendChild( letStyle );
+			letElement.appendChild( letStyle );
 		} // end if
+
+		return true;
+	}; // end of function
+
+	// add css
+	function fncShareCSS( ) {
+		let letString = '';
+		let letImgUp;
+		let letImgDown;
+		let letImgCheck; // beta version only
+
+		// img vs button
+		letImgUp = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAkUExURaqqqqqqqkdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqjfXEQEAAAAMdFJOU/wPAKSPdF28R8805OPAZQgAAABpSURBVBjTZZBbDgAhCANbfOv977urBJ/9sRkIkwh5AivkjdgaT8TSWuGBEmpF2pFDFslwC3nE/kR4QwFBjaN0NIZqHOvQE2bsR6GiafwLnA1t3YE8jeQ0itxGzW60bMaZaVwx48GI96M/9sgD7Y3Hro8AAAAASUVORK5CYII=';
+		letImgDown = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUBAMAAAByuXB5AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAhUExURUdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqu6rUBMAAAALdFJOUwAQ/j2n1PG8j1x0dBa5ZQAAAGhJREFUCNdjYIADQTjtKgBmMIYwmBWAWezJDMwqYJaTAQPDpAYgg0MTSHBpAYlFC0B6miYwcGqA9bIoMxg5QPSWGadD9YqmBUL1MoYKwPQKgvUyQPWCAFgvxBkgvRAA0gsBQL3oroUCAIEJErtmb0XIAAAAAElFTkSuQmCC';
+		letImgCheck = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsEAAA7BAbiRa+0AAABTSURBVDhPY/z//z8DNQATlKYYEDRo9erV/0EYysUJqOaiIRhGxAKsXsMWuKGhoYxQJlZAv8CGuY5uLho1iDAYfAYRnbJhAFd6ornX2PFgLICBAQBQciFGldEVwAAAAABJRU5ErkJggg=='; // beta version only
+		// button id
+		letString += '#play_btn_up { position:fixed; right:0; bottom:' + POSITION + '%; z-index:' + LAYER_INDEX + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgUp + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
+		letString += '#play_btn_dn { position:fixed; right:0; top:' + POSITION + '%; z-index:' + LAYER_INDEX + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgDown + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
+		letString += '#play_btn_chk { position:fixed; right:0; top:' + ( POSITION + POSITION_OFFSET ) + '%; z-index:' + LAYER_INDEX + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgCheck + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; // beta version only
+		// button class
+		letString += '.play_btn { -webkit-transition-duration:0.5s linear; -o-transition-duration:0.5s linear; -moz-transition-duration:0.5s linear; transition-duration:0.5s linear; opacity:0.65; }'; 
+		letString += '.play_btn:hover { opacity:1; }'; 
+		// append
+		fncAddStyle( '' + letString );
 
 		return true;
 	}; // end of function
 
 	// move up
 	function fncMoveUp( ) { 
-		window.scrollBy( 0, -fncScrollTop( LOADING_ELEMENT ) );
+		window.scrollBy( 0, -fncScrollTop( LOAD_ELEMENT ) );
 
 		return true;
 	}; // end of function
 
 	// move down
 	function fncMoveDown( ) { 
-		window.scrollBy( 0, fncScrollBottom( LOADING_ELEMENT ) );
+		window.scrollBy( 0, fncScrollBottom( LOAD_ELEMENT ) );
 
 		return true;
 	}; // end of function
 
-	// get y-position from top to client top
+	// get position from top to client top
 	function fncScrollTop( varDocumentElement ) { return window.pageYOffset || varDocumentElement.scrollTop; }; // end of function
 
-	// get y-position from bottom to client bottom
+	// get position from bottom to client bottom
 	function fncScrollBottom( varDocumentElement ) { return varDocumentElement.scrollHeight - varDocumentElement.clientHeight - fncScrollTop( varDocumentElement ); }; // end of function
 
+	// compare height
+	function fncCompareHeight( ) { return ( LOAD_ELEMENT.clientHeight < LOAD_ELEMENT.scrollHeight ); }; // end of function
+	
 	// Height check		// only beta version
 	function fncCheckHeight( ) {
 		let letAlert = "";
@@ -132,33 +159,6 @@ function SetTopBottomButtons( ) {
 		return true;
 	}; // end of function
 
-	// compare height
-	function fncCompareHeight( ) { return ( LOADING_ELEMENT.clientHeight < LOADING_ELEMENT.scrollHeight ); }; // end of function
-
-	// add css
-	function fncShareCSS( ) {
-		let letString = '';
-		let letImgUp;
-		let letImgDown;
-		let letImgCheck; // beta version only
-
-		// img vs button
-		letImgUp = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAMAAAC3SZ14AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAkUExURaqqqqqqqkdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqjfXEQEAAAAMdFJOU/wPAKSPdF28R8805OPAZQgAAABpSURBVBjTZZBbDgAhCANbfOv977urBJ/9sRkIkwh5AivkjdgaT8TSWuGBEmpF2pFDFslwC3nE/kR4QwFBjaN0NIZqHOvQE2bsR6GiafwLnA1t3YE8jeQ0itxGzW60bMaZaVwx48GI96M/9sgD7Y3Hro8AAAAASUVORK5CYII=';
-		letImgDown = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUBAMAAAByuXB5AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAhUExURUdwTKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqu6rUBMAAAALdFJOUwAQ/j2n1PG8j1x0dBa5ZQAAAGhJREFUCNdjYIADQTjtKgBmMIYwmBWAWezJDMwqYJaTAQPDpAYgg0MTSHBpAYlFC0B6miYwcGqA9bIoMxg5QPSWGadD9YqmBUL1MoYKwPQKgvUyQPWCAFgvxBkgvRAA0gsBQL3oroUCAIEJErtmb0XIAAAAAElFTkSuQmCC';
-		letImgCheck = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAAUCAYAAACAl21KAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsEAAA7BAbiRa+0AAABTSURBVDhPY/z//z8DNQATlKYYEDRo9erV/0EYysUJqOaiIRhGxAKsXsMWuKGhoYxQJlZAv8CGuY5uLho1iDAYfAYRnbJhAFd6ornX2PFgLICBAQBQciFGldEVwAAAAABJRU5ErkJggg=='; // beta version only
-		// button id
-		letString += '#play_btn_up { position:fixed; right:0; bottom:' + Y_POSITION + '%; z-index:' + LAYER_INDEX + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgUp + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
-		letString += '#play_btn_dn { position:fixed; right:0; top:' + Y_POSITION + '%; z-index:' + LAYER_INDEX + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgDown + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; 
-		letString += '#play_btn_chk { position:fixed; right:0; top:' + ( Y_POSITION + Y_POSITION_OFFSET ) + '%; z-index:' + LAYER_INDEX + '; height:36px; width:36px; cursor:pointer; background:url(' + letImgCheck + ') no-repeat scroll 50% 50% rgba( 0, 0, 0, 0.7 ); border-radius:5px 0 0 5px; margin-top:-24px; }'; // beta version only
-		// button class
-		letString += '.play_btn { -webkit-transition-duration:0.5s linear; -o-transition-duration:0.5s linear; -moz-transition-duration:0.5s linear; transition-duration:0.5s linear; opacity:0.65; }'; 
-		letString += '.play_btn:hover { opacity:1; }'; 
-		// append
-		fncAddStyle( '' + letString );
-
-		return true;
-	}; // end of function
-
 	// main
 	function fncCreateButtonElement( ) { 
 		let letUpButton;
@@ -168,9 +168,9 @@ function SetTopBottomButtons( ) {
 		// switch visible buttons
 		function fncVisibleButtons( ) {
 			// if scroll up
-			letUpButton.style.display = ( fncScrollTop( LOADING_ELEMENT ) > 0 ) ? "" : "none";
+			letUpButton.style.display = ( fncScrollTop( LOAD_ELEMENT ) > 0 ) ? "" : "none";
 			// if scroll down
-			letDownButton.style.display = ( fncScrollBottom( LOADING_ELEMENT ) >= 1 ) ? "" : "none"; // remove digits after decimal point
+			letDownButton.style.display = ( fncScrollBottom( LOAD_ELEMENT ) >= 1 ) ? "" : "none"; // remove digits after decimal point
 			// always on
 			letCheckButton.style.display = ""; // beta version only
 
@@ -188,22 +188,22 @@ function SetTopBottomButtons( ) {
 			return true;
 		}; // end function
 
-		// reset timer
+		// restart timer
 		function fncRestartTimer( ) {
 			clearTimeout( letIdleTimer );
-			letIdleTimer = setTimeout( fncInvisibleButtons, IDLE_TIME_OUT );
+			letIdleTimer = setTimeout( fncInvisibleButtons, IDLE_TIMEOUT );
 
 			return true;
 		}; // end function
 
-		// exit function
+		// if unnecessary
 		if( !fncCompareHeight( ) ) { return; }; // end if
 
 		// add css
 		fncShareCSS( ); 
 		
-		// if loading element
-		if( LOADING_ELEMENT ) { 
+		// if load element
+		if( LOAD_ELEMENT ) { 
 			// create DOM element
 			letUpButton = fncCreateElement( 'span' );
 			letDownButton = fncCreateElement( 'span' );
@@ -224,7 +224,7 @@ function SetTopBottomButtons( ) {
 			// initialize buttons
 			fncVisibleButtons( );
 
-			// add event loading
+			// add event load
 			window.addEventListener( 'load', fncRestartTimer, false );
 			// add event click up button
 			letUpButton.addEventListener( 'click', fncMoveUp, false );
